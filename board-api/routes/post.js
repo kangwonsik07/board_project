d523edf436c08c6781ac0a6496ecf9cf4d1b0f3f
kangwonsik07 <alucard815@naver.com>
2025-01-09 18:03:43 +0900

    up

--- board-api/routes/post.js
+++ board-api/routes/post.js
@@ -3,6 +3,7 @@ const { isLoggedIn } = require('./middlewares')
 const { Post, User, Board } = require('../models/')
 const router = express.Router()
 
+// 게시물 작성
 router.post('/create', isLoggedIn, async (req, res) => {
    try {
       const post = await Post.create({
@@ -32,6 +33,7 @@ router.post('/create', isLoggedIn, async (req, res) => {
    }
 })
 
+// 수정
 router.put('/:id', isLoggedIn, async (req, res) => {
    try {
       const post = await Post.findOne({ where: { id: req.params.id, UserId: req.user.id } })
@@ -56,16 +58,17 @@ router.put('/:id', isLoggedIn, async (req, res) => {
       })
 
       res.json({
-         succcess: true,
+         success: true,
          post: updatePost,
          message: '게시물이 성공적으로 수정되었습니다.',
       })
    } catch (error) {
       console.error(error)
-      res.status(500).json({ succcess: false, message: '게시물 수정 중 오류 발생', error })
+      res.status(500).json({ success: false, message: '게시물 수정 중 오류 발생', error })
    }
 })
 
+// 삭제
 router.delete('/:id', isLoggedIn, async (req, res) => {
    try {
       const post = await Post.findOne({ where: { id: req.params.id, UserId: req.params.id } })
@@ -76,12 +79,50 @@ router.delete('/:id', isLoggedIn, async (req, res) => {
       await post.destroy()
 
       res.json({
-         succcess: true,
+         success: true,
          message: '게시물이 삭제 되었습니다',
       })
    } catch (error) {
       console.error(error)
-      res.status(500).json({ succcess: false, message: '게시물 삭제 중 오류 발생', error })
+      res.status(500).json({ success: false, message: '게시물 삭제 중 오류 발생', error })
+   }
+})
+
+// 게시물 불러오기
+router.get('/', async (req, res) => {
+   try {
+      const page = parseInt(req.query.page, 10) || 1
+      const limit = parseInt(req.query.limit, 15) || 10
+      const offset = (page - 1) * limit
+
+      const count = await Post.count()
+
+      const posts = await Post.findAll({
+         limit,
+         offset,
+         order: [['createdAt', 'DESC']],
+         include: [
+            {
+               model: User,
+               attributes: ['id', 'nick', 'email'],
+            },
+         ],
+      })
+
+      res.json({
+         success: true,
+         posts,
+         pagination: {
+            totalPosts: count,
+            currentPage: page,
+            totalPages: Math.ceil(count / limit),
+            limit,
+         },
+         message: '전체 게시물 리스트를 성공적으로 불러왔습니다',
+      })
+   } catch (error) {
+      console.error(error)
+      res.status(500).json({ success: false, message: '게시물 리스트를 불러오는 중 오류가 발생했습니다.', error })
    }
 })
 
